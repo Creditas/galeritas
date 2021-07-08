f648ab02bae2127f821947a39f5fa47983002e89
Aurelio Saraiva <aureliosaraiva@gmail.com>
2021-07-08 09:48:30 -0300

    upload pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
+import sys
+import os
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
+VERSION = "0.1.0"
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 setup(
     name='galeritas',
-    version='0.1.0',
+    version=VERSION,
     packages=find_packages(where='galeritas'),
     package_dir={'': 'galeritas'},
     python_requires='>=3.6.9',
@@ -25,5 +43,11 @@ setup(
     author_email='data-science@creditas.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    classifiers=['Programming Language :: Python :: 3.6.9']
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
