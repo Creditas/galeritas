4648e46ae7eef7f98fff9bbddd4bbca9f1521842
vivianyamassaki <vivian.yamassaki@creditas.com.br>
2021-07-12 16:35:09 -0300

    try RTD to generate documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,19 +13,7 @@
 import os
 import sys
 
-import mock
-
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'pandas', 'scikit-learn', 'seaborn']
-
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
-
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-
 sys.path.insert(0, os.path.abspath("../../"))
-package_path = os.path.abspath('../../')
-os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
 # -- Project information -----------------------------------------------------
 
