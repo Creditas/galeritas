8a1c5f0ca5bac44d0559fee9ccadc2de9517afef
vivianyamassaki <vivian.yamassaki@creditas.com.br>
2021-07-12 15:52:12 -0300

    try RTD to generate documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,11 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../'))
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+
+sys.path.insert(0, os.path.abspath("../../"))
 package_path = os.path.abspath('../../')
 os.environ['PYTHONPATH'] = ':'.join((package_path, os.environ.get('PYTHONPATH', '')))
 
