f241dd8738a56ae6aebb7ddcf701a12a2d3a33c9
vivianyamassaki <vivian.yamassaki@creditas.com.br>
2021-07-12 15:58:26 -0300

    add mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,12 @@
 import os
 import sys
 
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'pandas', 'scikit-learn', 'seaborn']
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
