a322f2a55186d630f5391aa2e0773f690cd1d5a8
joaoOarthur <joao.arthurt@gmail.com>
2021-10-20 15:41:10 -0300

    include plot_ks_classification function and tests

--- galeritas/__init__.py
+++ galeritas/__init__.py
@@ -2,12 +2,14 @@ import seaborn as sns
 from .stacked_percentage_bar_plot import *
 from .plot_ecdf_curve import *
 from .bar_plot_with_population_proportion import *
-from. precision_and_recall_by_probability_threshold import *
+from .precision_and_recall_by_probability_threshold import *
+from .plot_ks_classification import *
 
 __all__ = ["stacked_percentage_bar_plot",
            "bar_plot_with_population_proportion",
            "plot_ecdf_curve",
-           "plot_precision_and_recall_by_probability_threshold"]
+           "plot_precision_and_recall_by_probability_threshold",
+           "plot_ks_classification"]
 
 sns.set_style("darkgrid")
 
