293534358658c680c4c92c5499663ebadff826a8
Luana Moreira <oliveira.m.luana@gmail.com>
2021-12-10 09:45:03 -0300

    calibration and distribution graph

--- galeritas/__init__.py
+++ galeritas/__init__.py
@@ -4,12 +4,14 @@ from .plot_ecdf_curve import *
 from .bar_plot_with_population_proportion import *
 from .precision_and_recall_by_probability_threshold import *
 from .plot_ks_classification import *
+from .plot_calibration_and_distribution import *
 
 __all__ = ["stacked_percentage_bar_plot",
            "bar_plot_with_population_proportion",
            "plot_ecdf_curve",
            "plot_precision_and_recall_by_probability_threshold",
-           "plot_ks_classification"]
+           "plot_ks_classification",
+           "plot_calibration_and_distribution"]
 
 sns.set_style("darkgrid")
 
