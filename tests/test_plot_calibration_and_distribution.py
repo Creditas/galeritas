dfbc2f7ff1de3f430d65df042a6e6ac4cedfd541
joaoOarthur <joao.arthurt@gmail.com>
2021-12-29 16:01:29 -0300

    update tests calibration

--- tests/test_plot_calibration_and_distribution.py
+++ tests/test_plot_calibration_and_distribution.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
 
+from matplotlib import pyplot as plt
 from galeritas import plot_calibration_and_distribution
 
 
@@ -18,6 +19,16 @@ def test_should_generate_plot_calibration_and_distribution_correctly(load_data):
 
     return plot_calibration_and_distribution(df, 'y_true', 'y_pred', return_fig=True)
 
+@pytest.mark.mpl_image_compare
+def test_should_generate_subplot_with_calibration_and_distribution_correctly(load_data):
+    df = load_data
+
+    f, axes = plt.subplots(1,2)
+
+    plot_calibration_and_distribution(df, 'y_true', 'y_pred', ax=axes[0])
+
+    return f
+
 
 def test_should_return_type_error_when_a_parameter_is_missing(load_data):
     df = load_data
@@ -31,3 +42,14 @@ def test_should_raise_error_when_strategy_is_invalid(load_data):
 
     with pytest.raises(ValueError):
         plot_calibration_and_distribution(df, 'y_true', 'y_pred', strategy='alo', return_fig=True)
+
+@pytest.mark.mpl_image_compare
+def test_should_raise_warning_when_ax_and_show_distribution_passed(load_data):
+    df = load_data
+    
+    f, axes = plt.subplots(1,2)
+
+    with pytest.warns(UserWarning):
+        plot_calibration_and_distribution(df, 'y_true', 'y_pred', show_distribution=True, ax=axes[0])
+
+    return f
